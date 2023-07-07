89c68a0c5ee29363b11d48468e2e3520bb533e50
Chakrit Tangpanupong <86833338+BIOPHARM101@users.noreply.github.com>
2023-07-08 00:06:15 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ ffmpeg.setFfmpegPath(ffmpegPath)
 
 const { v4: uuidv4 } = require("uuid");
 const app = express();
-const port = process.env.PORT || 69420;
+const port = process.env.PORT || 42069;
 app.listen(port);
 app.use(bodyParser.json())
 app.use(cors());
